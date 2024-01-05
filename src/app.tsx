ebb45eae45484735c51f2997b1376ef112d683c6
JayaSurya12345 <jayasurya.n2021csea@sece.ac.in>
2024-01-05 09:43:32 +0530

    Mine Sweeper

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 
 import './app.css'
 
 type Cell = {
   isMine: boolean
+  mineCount?: number
+  isRevealed: boolean
 }
 
 export function App() {
 
   const [board, setBoard] = useState<Array<Array<Cell>>>([])
+
+  const [isGameOver, setIsGameOver] = useState<boolean>(false)
+
+  useEffect(() => {
+    createBoard()
+  }, [])
 
   let numRows = 8
   let numCols = 10
@@ -41,31 +49,132 @@ export function App() {
       let currRow: Array<Cell> = []
       for (let j = 0; j < numCols; j++) {
         let isMine = mineCells.includes(i * numCols + j)
-        currRow.push({ isMine })
+        currRow.push({
+          isMine,
+          isRevealed: false
+        })
       }
 
       newBoard.push(currRow)
     }
 
+    for (let i = 0; i < numRows; i++) {
+      for (let j = 0; j < numCols; j++) {
+        if (newBoard[i][j].isMine) {
+          continue
+        }
+
+        let mineCount = 0
+        for (let di = -1; di <= 1; di++) {
+          for (let dj = -1; dj <= 1; dj++) {
+            let [newI, newJ] = [i + di, j + dj]
+            if (
+              newI < 0 || newI >= numRows ||
+              newJ < 0 || newJ >= numCols
+            ) {
+              continue
+            }
+            
+            if (newBoard[newI][newJ].isMine) {
+              mineCount++
+            }
+          }
+        }
+
+        newBoard[i][j].mineCount = mineCount
+      }
+    }
+
     setBoard(newBoard)
   }
 
-  function getRow() {
-    return (
-      <div className="row">
-        {
-          new Array(numCols).fill(0).map(
-            _ => <div className="cell"> * </div>
-          )
-        }
-      </div>
-    )
+  // Flood fill algorithm
+  function revealRecursive(i: number, j: number, board: Cell[][]) {
+    if (i < 0 || j < 0 || i >= numRows || j >= numCols) return;
+    if (board[i][j].isMine) return;
+    if (board[i][j].isRevealed) return;
+
+    board[i][j].isRevealed = true;
+    if (board[i][j].mineCount != 0) return;
+
+    for (let di = -1; di <= 1; di++) {
+      for (let dj = -1; dj <= 1; dj++) {
+        // Below condition not required
+        // if (di == 0 && dj == 0) continue;
+        revealRecursive(i+di, j+dj, board)
+      }
+    }
+  }
+
+  function handleCellClick(i: number, j: number) {
+
+    if (isGameOver) return;
+
+    let newBoard = [...board]
+
+    if (newBoard[i][j].isMine) {
+      alert("Game over!")
+
+      // Method 1
+      // for (let i = 0; i < numRows; i++) {
+      //   for (let j = 0; j < numCols; j++) {
+      //     newBoard[i][j].isRevealed = true
+      //   }
+      // }
+
+      setIsGameOver(true);      
+    } else {
+      if (newBoard[i][j].mineCount == 0) {
+        revealRecursive(i, j, newBoard)
+      }
+    }
+
+    newBoard[i][j].isRevealed = true
+
+    setBoard(newBoard)
+  }
+
+  function display(cell: Cell) {
+    // if (!cell.isRevealed) return "";
+
+    // method 2: use isGameOver
+    if (!isGameOver && !cell.isRevealed) return "";
+    if (cell.isMine) return "💣";
+    return cell.mineCount || 0;
   }
 
   return (
     <div id="app">
       {
-        new Array(numRows).fill(0).map(_ => getRow())
+        board.map(
+          (row, i) => (
+            <div className="row">
+              {
+                row.map(
+                  (cell, j) => (
+                    <div
+                      className="cell"
+                      style={{
+                        cursor: (cell.isRevealed || isGameOver)
+                          ? "default"
+                          : "pointer",
+
+                        backgroundColor: (cell.isRevealed || isGameOver)
+                          ? "#fff"
+                          : "#0ba"
+                      }}
+                      onClick={
+                        () => handleCellClick(i, j)
+                      }
+                      >
+                      { display(cell) }
+                    </div>
+                  )
+                )
+              }
+            </div>
+          )
+        )
       }
     </div>
   )
